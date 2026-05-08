499c74549b9f88f30e2557f774fcc8233f9e09b2
Hamid <hamid@logicalclocks.com>
2026-05-08 17:10:03 +0200

    [HWORKS-2770] More fixes on broken scala tests

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveDDLSuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveDDLSuite.scala
@@ -2811,10 +2811,13 @@ class HiveDDLSuite
             checkAnswer(spark.table("t"), Row(1))
             val maybeFile = path.listFiles().find(_.getName.startsWith("part"))
 
-            Utils.tryWithResource(getReader(maybeFile.head.getCanonicalPath)) { reader =>
+            val reader = getReader(maybeFile.head.getCanonicalPath)
+            try {
               assert(reader.getCompressionKind.name === "ZLIB")
               assert(reader.getCompressionSize == 1001)
               assert(reader.getRowIndexStride == 2002)
+            } finally {
+              reader.close()
             }
           }
         }
